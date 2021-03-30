c1e416f8e4c9c5804e66ab83dfb70f3a91feffc3
mvictor10 <mvictordossantos.limite@gmail.com>
2021-03-29 21:34:36 -0300

    Add files via upload

---
+++ api.php
@@ -0,0 +1,17 @@
+{
+  "1": {
+    "id": 1,
+    "name": "VIKTOR KRAUSS",
+    "age": 21
+  },
+  "2": {
+    "id": 2,
+    "name": "VIKTHORYA KRAUSS",
+    "age": 19
+  },
+  "3": {
+    "id": 3,
+    "name": "PETER JORDAN",
+    "age": 26
+  }
+}
